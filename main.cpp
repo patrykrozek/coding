77dbb83d834b40b62eeadac865f866ae0d78ae7d
Rozek <vboxuser@Xubuntu.myguest.virtualbox.org>
2022-10-31 17:46:34 +0100

    Moj pierwszy commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Hello World
+
+#include <iostream>
+
+int main ()
+{
+
+std::string lolo{"Hello World"};
+std::cout << lolo <<std::endl;
+//std::cout << "Hello World \n" ;
+
+return 0;
+}
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm ./bin/Hello
+g++ main.cpp -o ./bin/Hello
+./bin/Hello
+
