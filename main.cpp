8f23b9b353ba4139aa80fc7464c4b182ce00a890
Rozek <vboxuser@Xubuntu.myguest.virtualbox.org>
2022-11-07 16:35:33 +0100

    Moj drugi commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 
 int main ()
 {
-
+int wynik; 
+wynik=10+8;
+std::cout << wynik <<std::endl;
 std::string lolo{"Hello World"};
 std::cout << lolo <<std::endl;
 //std::cout << "Hello World \n" ;
+
+int wynik2 = 10+"a"; 
+std::cout << wynik2 <<std::endl;
+
 
 return 0;
 }
